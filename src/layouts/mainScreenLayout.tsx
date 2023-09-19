abf9551e9a2822046591e2e41ffd9826fa2fdb98
Irina Mandarina <irina.ahamad@gmail.com>
2023-09-19 22:17:02 +0200

    demo

--- src/layouts/mainScreenLayout.tsx
+++ src/layouts/mainScreenLayout.tsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./gradient.css";
+import {getRandomInt} from "../services/random";
 
 const Layout: React.FC<{ children: any }> = (props) => {
     const [meshes, setMeshes] = useState([] as Mesh[]);
     const meshCount = 5;
     const meshColours: string[] = [
-        "#18966c",
+        "#0c563e",
         "#185c45",
         "#184c5c",
-        "#06a2d1",
-        "#6ad6f7",
+        "rgba(6,162,209,0.68)",
+        "rgba(67,165,196,0.83)",
         "#171b82",
         "#4f52b8",
-        "#248753",
+        "rgba(28,110,67,0.82)",
         "#085940",
         "#1b4f4d",
+        "#0a3548",
     ];
 
     useEffect(() => {
         const initMeshes = [];
         for (let count = 0; count < meshCount; count++) {
-            let randomWidth = Math.floor(Math.random() * 300);
+            let randomWidth = getRandomInt(200, 500)
             initMeshes.push({
                 width: randomWidth,
                 height: randomWidth,
-                x: Math.floor(Math.random() * 100),
-                y: Math.floor(Math.random() * 100),
+                x: getRandomInt(1, 100),
+                y: getRandomInt(1, 100),
                 colour: meshColours[Math.floor(Math.random() * meshColours.length)],
             });
         }
@@ -37,11 +39,11 @@ const Layout: React.FC<{ children: any }> = (props) => {
             setMeshes((prevMeshes) =>
                 prevMeshes.map((mesh) => ({
                     ...mesh,
-                    x: Math.floor(Math.random() * 100),
-                    y: Math.floor(Math.random() * 100),
+                    x: getRandomInt(1, 100),
+                    y: getRandomInt(1, 100),
                 }))
             );
-        }, 1000); // Decreased the interval to 1 second for testing
+        }, 10000); // Decreased the interval to 1 second for testing
 
         return () => clearInterval(intervalId);
     }, []);
@@ -49,10 +51,11 @@ const Layout: React.FC<{ children: any }> = (props) => {
     return (
         <div className="w-full h-full">
             <div
-                className="absolute w-full h-full"
+                className="fixed absolute w-full h-full -z-100"
                 style={{
                     top: 0,
                     left: 0,
+                    backgroundColor: '#0a3548'
                 }}
             >
                 {meshes.map((mesh, index) => (
@@ -65,9 +68,22 @@ const Layout: React.FC<{ children: any }> = (props) => {
                             height: `${mesh.height}px`,
                             borderRadius: "100%",
                             transform: `translate(${mesh.x}%, ${mesh.y}%)`, // Removed the *0.01 multiplier
-                            transition: "transform 1s", // Added a transition for smooth movement
+                            transition: "transform 9s ease-in-out", // Added a transition for smooth movement
+                            filter: 'blur(1.5rem)'
                         }}
-                    ></div>
+                    >
+                        <div style={{
+                            backgroundColor: mesh.colour,
+                            position: "absolute",
+                            width: `${mesh.width}px`,
+                            height: `${mesh.height}px`,
+                            borderRadius: "100%",
+                            transform: `translate(${mesh.x}%, ${mesh.y}%)`, // Removed the *0.01 multiplier
+                            transition: "transform 9s ease-in-out", // Added a transition for smooth movement
+                            filter: 'blur(1.5rem)'
+                        }}>
+                        </div>
+                    </div>
                 ))}
             </div>
 
